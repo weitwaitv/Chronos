af5db3ce1d13f2c35f50f9d793dedb36d4c66230
tanjiahao <tanjiahao@laiye.com>
2023-12-21 18:41:17 +0800

    feat: modify bug

--- core/evals/module/data_convert.py
+++ core/evals/module/data_convert.py
@@ -1,8 +1,10 @@
+import os
 from pathlib import Path
 from typing import List
 
 from core.evals.module.base import Sample
 from modules.file_process import get_json, get_jsonl
+from modules.helper import file_path
 
 
 def get_input_data(path: str) -> List[Sample]:
@@ -10,8 +12,10 @@ def get_input_data(path: str) -> List[Sample]:
     :param path:
     :return:
     """
-    file_path = Path(path)
-    file_extension = file_path.suffix
+    file_extension = Path(path).suffix
+    is_absolute: bool = os.path.isabs(path)
+    if not is_absolute:
+        path = file_path(path)
     if file_extension == ".jsonl":
         file_data = get_jsonl(path)
     elif file_extension == ".json":
